38506cf391acf9090a03d5f523e801137d4ff53d
Manuel Kuchelmeister <manuel.kuchelmeister@web.de>
2022-02-21 11:20:18 +0200

    Add mosaic plot for quantum chemistry results

--- scripts/plot_tran_learn_gain.py
+++ scripts/plot_tran_learn_gain.py
@@ -9,12 +9,12 @@ from pathlib import Path
 PROJECT_DIR = Path(__file__).resolve().parent.parent
 RESULTS_DIR = PROJECT_DIR / 'results/'
 
-FIG_LABELS = {'LF ➔ HF': ['a)', 'b)'],
-                 'LF ➔ UHF': ['c)', 'd)'],
-                 'HF ➔ UHF': ['e)', 'f)']}
+# 'LF ➔ HF': ['a)', 'b)'],
+FIG_LABELS = {'LF ➔ UHF': ['a)', 'b)'],
+              'HF ➔ UHF': ['c)', 'd)']}
 BLUE, RED = '#000082', '#FE0000'
 SCATTER_DICT = {'color': BLUE, 'alpha': .4, 'marker': 'x',
-                'label': 'observation', 's': 60}
+                'label': 'CCSD(T) run', 's': 60}
 MEANS_DICT = {'color': RED, 'marker': '*', 's': 120, 'label': 'mean'}
 SMALL_SIZE, MEDIUM_SIZE, LARGE_SIZE = 14, 18, 22
 mpl.rc('xtick', labelsize=SMALL_SIZE)
@@ -29,21 +29,13 @@ def main(args):
                     'Initial points source 1', f'Iterations [{tol} kcal/mol]',
                     f'Totaltime [{tol} kcal/mol]']]
     df = df.rename(columns={f'Iterations [{tol} kcal/mol]': 'Iterations',
-                       f'Totaltime [{tol} kcal/mol]': 'Totaltime'})
-    tmp_df = df.loc[df['Initial points source 1'] != 0, ['Iterations']]
-    tmp_df.to_csv('tmp.csv')
+                   f'Totaltime [{tol} kcal/mol]': 'Totaltime'})
     plot_tl_results(df)
 
 
 def plot_tl_results(df):
-    fig, axs = plt.subplot_mosaic([['a)', 'c)', 'e)'], ['b)', 'd)', 'f)']],
-                                  figsize=(8, 5), sharex=True)
-    for label, ax in axs.items():
-        # label physical distance in and down:
-        trans = mtransforms.ScaledTranslation(-25/72, -7/72,
-                                              fig.dpi_scale_trans)
-        ax.text(1.0, 1.0, label, transform=ax.transAxes + trans,
-                fontsize=18, verticalalignment='top')
+    fig, axs = plt.subplot_mosaic([['a)', 'c)'], ['b)', 'd)']],
+                                  figsize=(5, 5), sharex=True)
     means = {}
     for setup in FIG_LABELS:
         means[setup] = {}
@@ -55,40 +47,42 @@ def plot_tl_results(df):
                 df['Experiment name'] == setup]['Initial points source 1'])
             y_bl = df.loc[df['Experiment name'] == baseline_setup][measure]
             y_tl = df.loc[df['Experiment name'] == setup][measure]
-            if measure == 'Totaltime':
-                y_bl, y_tl = y_bl/3600, y_tl/3600
             x_bl = np.zeros_like(y_bl)
             for x, y in zip([x_bl, x_tl], [y_bl, y_tl]):
                 tmp_df = pd.DataFrame({'x': x, 'y': y})
                 x_means = np.array(tmp_df['x'].unique())
-                y_means = np.array([tmp_df.loc[tmp_df['x'] == x_mean, ['y']].mean()
-                                    for x_mean in x_means])
+                y_means = np.array(
+                    [tmp_df.loc[tmp_df['x'] == x_mean, ['y']].mean()
+                     for x_mean in x_means])
                 for x_mean, y_mean in zip(x_means, y_means):
                     x_mean = x_mean.squeeze()
                     y_mean = y_mean.squeeze()
                     means[setup][measure].append([int(x_mean), float(y_mean)])
                 axs[ax_label].scatter(x, y, **SCATTER_DICT)
                 axs[ax_label].scatter(x_means, y_means, **MEANS_DICT)
-
-            if measure == 'Totaltime':
-                if ax_label == 'b)':
-                    axs[ax_label].set_ylabel('CPU time [h]')
-                axs[ax_label].set_xlabel('Number of Lower-Fi Samples')
-            elif (measure == 'Iterations') and (ax_label == 'a)'):
-                axs[ax_label].set_ylabel('BOSS iterations')
     for setup in means:
-        for measure in means[setup]:
+        for measure, ax_label in zip(means[setup], FIG_LABELS[setup]):
             tmp = means[setup][measure]
             tmp = np.array(tmp)
             tmp = tmp[tmp[:, 0].argsort()]
-            print(setup, measure)
-            FIG_LABELS[setup]
-            if measure == 'Totaltime':
-                print(tmp[:, 1] / tmp[0, 1])
-    fig.suptitle('Cost for global minimum inference\nup to 0.1 kcal/mol accuracy',
-                 fontsize=MEDIUM_SIZE)
+            axs[ax_label].plot(tmp[:, 0], tmp[:, 1], zorder=-10, c=RED,
+                               ls='dashed')
+
+    axs['a)'].set_title('LF ➔ UHF', fontsize=SMALL_SIZE)
+    axs['a)'].set_ylabel('BOSS iterations', fontsize=SMALL_SIZE)
+    axs['b)'].set_ylabel('CPU time [h]', fontsize=SMALL_SIZE)
+    axs['c)'].set_title('HF ➔ UHF', fontsize=SMALL_SIZE)
+    axs['c)'].set_yticks([])
+    axs['d)'].set_yticks([])
+    handles, labels = plt.gca().get_legend_handles_labels()
+    by_label = dict(zip(labels, handles))
+    axs['d)'].legend(by_label.values(), by_label.keys(), fontsize=SMALL_SIZE,
+                     loc='lower left')
+    fig.supxlabel('Number of Lower-Fi Samples', fontsize=SMALL_SIZE)
     plt.tight_layout()
-    plt.savefig('results/tran_learn_gain.png')
+    fig.subplots_adjust(bottom=0.12)
+    plt.savefig('results/tran_learn_gain.pdf')
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
